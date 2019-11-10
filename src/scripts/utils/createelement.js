38a854ffb098001b7ebcbf9a15a08e89fdedecdb
Oskar Wróbel <kontakt@oskarwrobel.pl>
2019-11-11 00:33:24 +0100

    Added utils for creaing XML Elements.

---
+++ src/scripts/utils/createelement.js
@@ -0,0 +1,23 @@
+export function createElement( name, attributes ) {
+	return _createElement( false, name, attributes );
+}
+
+export function createSvgElement( name, attributes ) {
+	return _createElement( true, name, attributes );
+}
+
+function _createElement( isNameSpaced, name, attributes = {} ) {
+	let element;
+
+	if ( isNameSpaced ) {
+		element = document.createElementNS( 'http://www.w3.org/2000/svg', name );
+	} else {
+		element = document.createElement( name );
+	}
+
+	for ( const name of Object.keys( attributes ) ) {
+		element.setAttribute( name, attributes[ name ] );
+	}
+
+	return element;
+}
